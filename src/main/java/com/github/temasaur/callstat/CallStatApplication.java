e5b272b9deff34618063e97532b77cdbcb808da7
Artemy Vakhrushev <69428015+TemaSaur@users.noreply.github.com>
2025-03-18 13:28:51 +0500

    Initial commit

---
+++ src/main/java/com/github/temasaur/callstat/CallStatApplication.java
@@ -0,0 +1,13 @@
+package com.github.temasaur.callstat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallStatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CallStatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/temasaur/callstat/CallStatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.temasaur.callstat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallStatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
