1e1524091bc68d52b8997cdc48d19845286a509c
Artemy Vakhrushev <69428015+TemaSaur@users.noreply.github.com>
2025-03-22 23:18:44 +0500

    fix background task logic

--- src/main/java/com/github/temasaur/callstat/services/backgroundTask/BackgroundTaskService.java
+++ src/main/java/com/github/temasaur/callstat/services/backgroundTask/BackgroundTaskService.java
@@ -5,6 +5,8 @@ import java.util.UUID;
 import com.github.temasaur.callstat.models.BackgroundTask;
 
 public interface BackgroundTaskService {
-    void setStatus(UUID uuid, BackgroundTask.Status status);
-    String getStatus(UUID uuid);
+    BackgroundTask getState(UUID uuid);
+
+	void setState(UUID uuid, BackgroundTask state);
+    void setStatusMessage(UUID uuid, BackgroundTask.Status status, String message);
 }
--- src/main/java/com/github/temasaur/callstat/services/backgroundTask/backgroundTaskAbstractService.java
+++ src/main/java/com/github/temasaur/callstat/services/backgroundTask/backgroundTaskAbstractService.java
@@ -3,15 +3,21 @@ package com.github.temasaur.callstat.services.backgroundTask;
 import java.util.UUID;
 
 import com.github.temasaur.callstat.models.BackgroundTask;
+import com.github.temasaur.callstat.models.BackgroundTask.Status;
 
 public class BackgroundTaskAbstractService implements BackgroundTaskService {
     @Override
-    public String getStatus(UUID uuid) {
+    public BackgroundTask getState(UUID uuid) {
         throw new UnsupportedOperationException("Not implemented yet");
     }
 
     @Override
-    public void setStatus(UUID uuid, BackgroundTask.Status status) {
-        throw new UnsupportedOperationException("Not implemented yet");
+    public void setState(UUID uuid, BackgroundTask state) {
+        throw new UnsupportedOperationException("Unimplemented method 'setState'");
+    }
+
+    @Override
+    public void setStatusMessage(UUID uuid, Status status, String message) {
+        throw new UnsupportedOperationException("Unimplemented method 'setStatusMessage'");
     }
 }
